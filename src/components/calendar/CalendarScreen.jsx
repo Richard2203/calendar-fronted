c258611bb344ed3440d88240f229706aa07e75ef
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-22 20:44:58 -0500

    instalacion de calendar y moment. Agregando el calendario a CalendarScreen

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import NavBar from '../ui/NavBar';
+// el calendario proviene de una libreria instalada con
+// npm i react-big-calendar, las importaciones de la linea 5 y 6 son necesarias
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+//esta libreria nos permite la manipulacion de fechas
+import moment from 'moment';
+
+// para mostrar el contenido del calendario debemos aplicas estilos width y height
+// indicando que abarque el 100%
+import './calendar_styles.css';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+	{
+		title: 'Cumpleaños del jefe',
+		start: moment().toDate(),
+		end: moment().add(2, 'hours').toDate(),
+		bgColor: '#fafafa',
+	},
+];
 
 const CalendarScreen = () => {
 	return (
-		<div>
+		<div className="calendar-screen">
 			<NavBar />
+			<Calendar
+				localizer={localizer}
+				events={events}
+				startAccessor="start"
+				endAccessor="end"
+			/>
 		</div>
 	);
 };
