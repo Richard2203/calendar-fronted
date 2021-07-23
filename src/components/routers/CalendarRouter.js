2cb98f868c96e1cdd30e2cbd0ce7269c3877c8cb
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-22 19:29:57 -0500

    crecion de rutas login y calendarScreen

--- src/components/routers/CalendarRouter.js
+++ src/components/routers/CalendarRouter.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from 'react-router-dom';
+import CalendarScreen from '../calendar/CalendarScreen';
+import LoginScreen from '../auth/LoginScreen';
 
 const CalendarRouter = () => {
 	return (
 		<div>
-			<h2>Calendar Router</h2>
+			<Router>
+				<div>
+					<Switch>
+						<Route exact path="/">
+							<CalendarScreen />
+						</Route>
+						<Route exact path="/login">
+							<LoginScreen />
+						</Route>
+						<Redirect to="/" />
+					</Switch>
+				</div>
+			</Router>
 		</div>
 	);
 };
