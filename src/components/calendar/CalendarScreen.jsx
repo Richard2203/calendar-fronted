72a4986bc73ec801a8f101e1acb0703a6a0c8673
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-22 21:59:33 -0500

    aplicando estilos a un evento del calendario

--- src/components/calendar/CalendarScreen.jsx
+++ src/components/calendar/CalendarScreen.jsx
@@ -8,9 +8,13 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 //esta libreria nos permite la manipulacion de fechas
 import moment from 'moment';
 
+import { messages } from '../../helpers/calendar-message-es';
 // para mostrar el contenido del calendario debemos aplicas estilos width y height
 // indicando que abarque el 100%
-import './calendar_styles.css';
+import './calendar_styles.css'; // importando lenguaje espaniol para las fechas
+import 'moment/locale/es';
+
+moment.locale('es'); // estableciendo en espaniol la fechas
 
 const localizer = momentLocalizer(moment);
 
@@ -20,10 +24,25 @@ const events = [
 		start: moment().toDate(),
 		end: moment().add(2, 'hours').toDate(),
 		bgColor: '#fafafa',
+		notes: 'Comprarle un regalo',
 	},
 ];
 
 const CalendarScreen = () => {
+	// este metodo sera pasado a la propieadad "eventPropGetter"
+	// y el retorno de estilos seran los estilos que aplique a un evento
+	// en particular
+	const eventStyleGetter = (event, start, end, isSelected) => {
+		const style = {
+			backgroundColor: '#367CF7',
+			borderRadius: '0px',
+			opacity: 0.7,
+			display: 'block',
+			color: 'white',
+		};
+		return { style };
+	};
+
 	return (
 		<div className="calendar-screen">
 			<NavBar />
@@ -32,6 +51,8 @@ const CalendarScreen = () => {
 				events={events}
 				startAccessor="start"
 				endAccessor="end"
+				messages={messages}
+				eventPropGetter={eventStyleGetter}
 			/>
 		</div>
 	);
