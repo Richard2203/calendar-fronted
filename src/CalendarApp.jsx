1d2347b1d7c26435b1ab61f91ffd60601b17d887
Richard2203 <55164319+Richard2203@users.noreply.github.com>
2021-07-24 18:41:56 -0500

    instalacion de Redux y thunk, implementacion de Redux

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import CalendarRouter from './components/routers/CalendarRouter';
+import { store } from './store/store';
 
 const CalendarApp = () => {
 	return (
-		<div>
+		<Provider store={store}>
 			<CalendarRouter />
-		</div>
+		</Provider>
 	);
 };
 
